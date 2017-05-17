b40e588dc9ff5be65098d43ace20df532c5ea870
ssinghal16 <soniyasinghal1991@gmail.com>
2017-05-17 22:18:41 +0530

    Screenshot

--- Session1Assignment1.3/src/Assignment3.java
+++ Session1Assignment1.3/src/Assignment3.java
@@ -1,8 +1,11 @@
 //Write a program that swaps the values of two variables without using third variable.
 
+
 public class Assignment3 {
 
+	//Main method declaration 
 	public static void main(String[] args) {
+		
 		  int num1 = 20;
           int num2 = 30;
          
@@ -10,10 +13,12 @@ public class Assignment3 {
           System.out.println("Value of num1 is :" + num1);
           System.out.println("Value of num2 is :" +num2);
          
+          //Swapping of two variables without third variable 
           num1 = num1 + num2;
           num2 = num1 - num2;
           num1 = num1 - num2;
          
+          //Printing the values
           System.out.println("After Swapping");
           System.out.println("Value of num1 is :" + num1);
           System.out.println("Value of num2 is :" +num2);
